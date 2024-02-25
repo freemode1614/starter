8e336d60e850ebddbb695535cf25b03edfb78e23
freemode <freemode1614@outlook.com>
2024-02-25 20:21:49 +0800

    Update

--- lua/plugins/colorschemas.lua
+++ lua/plugins/colorschemas.lua
@@ -1,19 +1,35 @@
 return {
-  { "morhetz/gruvbox" },
+  {
+    "morhetz/gruvbox",
+  },
   { "sainnhe/gruvbox-material" },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "macchiato",
+      transparent_background = true,
+    },
+  },
   {
     "folke/tokyonight.nvim",
     opts = {
+      style = "night",
+      light_style = " day",
       transparent = true,
       styles = {
-        sidebar = "transparent",
+        siderbars = "transparent",
       },
+      dim_inactive = true,
+      lualine_bold = true,
+      siderbars = { "Neo-tree" },
     },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "catppuccin",
     },
   },
 }
