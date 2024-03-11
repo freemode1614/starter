1e8f8ec0cf79e18d2ccd925db70a29bad1d6268e
wp.l <wenpeng.lei@ncsi.com.cn>
2024-03-11 09:56:49 +0800

    Update plugin nvim-treesitter using a proxy for install

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,9 +1,11 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  config = function()
-    -- require("nvim-treesitter.install").prefer_git = true
-    require("nvim-treesitter.install").command_extra_args = {
-      curl = { "--proxy", "http://172.31.2.68:3128" },
-    }
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      -- require("nvim-treesitter.install").prefer_git = true
+      -- require("nvim-treesitter.install").command_extra_args = {
+      --   curl = { "--proxy", "http://172.31.2.68:3128" },
+      -- }
+    end,
+  },
 }
