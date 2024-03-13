7854031c6ff03aef423770f50b19a6fb84e7b1f0
wp.l <wenpeng.lei@ncsi.com.cn>
2024-03-13 09:24:36 +0800

    Remove symbols-outline

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,9 +3,9 @@ return {
     "nvim-treesitter/nvim-treesitter",
     config = function()
       -- require("nvim-treesitter.install").prefer_git = true
-      -- require("nvim-treesitter.install").command_extra_args = {
-      --   curl = { "--proxy", "http://172.31.2.68:3128" },
-      -- }
+      require("nvim-treesitter.install").command_extra_args = {
+        curl = { "--proxy", "http://172.31.2.68:3128" },
+      }
     end,
   },
 }
