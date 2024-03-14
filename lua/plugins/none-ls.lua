938205afb3a43db7de7b75ab4cd1d2ec3474d987
wp.l <wenpeng.lei@ncsi.com.cn>
2024-03-14 09:38:47 +0800

    设置 cspell 文件路径到 homedir/.cspell.json

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,9 +4,14 @@ return {
     dependencies = "davidmh/cspell.nvim",
     opts = function(_, opts)
       local cspell = require("cspell")
+      local config = {
+        find_json = function()
+          return vim.loop.os_homedir() .. "/.cspell.json"
+        end,
+      }
       opts.sources = vim.list_extend(opts.sources or {}, {
-        cspell.diagnostics,
-        cspell.code_actions,
+        cspell.diagnostics.with({ config = config }),
+        cspell.code_actions.with({ config = config }),
       })
     end,
   },
