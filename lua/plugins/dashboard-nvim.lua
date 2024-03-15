fe455b3c6026cbaf3a70b13dc8739e38c241e926
freemode <freemode1614@outlook.com>
2024-03-16 00:05:42 +0800

    update dahboard config

---
+++ lua/plugins/dashboard-nvim.lua
@@ -0,0 +1,55 @@
+return {
+  {
+    "nvimdev/dashboard-nvim",
+    opts = function(_, opts)
+      local logo = [[
+            _________ ________    _________ _________  _____   _____  ____  _______  _________ 
+            |_   __  ||_   __ \   |_   __  ||_   __  | |_   \  /   _|.'   `.|_   _ `.|_   __  | 
+              | |_ \_|  | |__) |    | |_ \_|  | |_ \_|   |   \/   | /  .-.  \ | | `. \ | |_ \_| 
+              |  _|     |  __ /     |  _| _   |  _| _    | |\  /| | | |   | | | |  | | |  _| _  
+             _| |_     _| |  \ \_  _| |__/ | _| |__/ |  _| |_\/_| |_\  `-'  /_| |_.' /_| |__/ | 
+            |_____|   |____| |___||________||________| |_____||_____|`.___.'|______.'|________| 
+                                                                                                
+    ]]
+
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+      opts.config.header = vim.split(logo, "\n")
+
+      opts.config.center = {
+        {
+          action = [[lua require("lazyvim.util").telescope.config_files()()]],
+          desc = " Config",
+          icon = " ",
+          key = "c",
+        },
+        {
+          action = 'lua require("persistence").load()',
+          desc = " Restore Session",
+          icon = " ",
+          key = "s",
+        },
+        {
+          action = "LazyExtras",
+          desc = " Lazy Extras",
+          icon = " ",
+          key = "x",
+        },
+        {
+          action = "Lazy",
+          desc = " Lazy",
+          icon = "󰒲 ",
+          key = "l",
+        },
+        {
+          action = "qa",
+          desc = " Quit",
+          icon = " ",
+          key = "q",
+        },
+      }
+
+      return opts
+    end,
+  },
+}
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
       -- require("nvim-treesitter.install").prefer_git = true
       require("nvim-treesitter.install").command_extra_args = {
-        curl = { "--proxy", "http://172.31.2.68:3128" },
+        -- curl = { "--proxy", "http://172.31.2.68:3128" },
       }
     end,
   },
