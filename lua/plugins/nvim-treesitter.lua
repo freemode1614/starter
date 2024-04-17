c6fcdb737096f4437835547933c6ea7fe73750f9
wp.l <wenpeng.lei@ncsi.com.cn>
2024-04-17 15:05:21 +0800

    Remove useless plugin file

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,9 +3,9 @@ return {
     "nvim-treesitter/nvim-treesitter",
     config = function()
       -- require("nvim-treesitter.install").prefer_git = true
-      require("nvim-treesitter.install").command_extra_args = {
-        curl = { "--proxy", "http://127.0.0.1:7897" },
-      }
+      -- require("nvim-treesitter.install").command_extra_args = {
+      --   curl = { "--proxy", "http://127.0.0.1:7897" },
+      -- }
     end,
   },
 }
