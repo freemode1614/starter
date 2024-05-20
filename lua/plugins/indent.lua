ee9a88169aa066649b9176f58516765199cb9d11
wp.l <wenpeng.lei@ncsi.com.cn>
2024-05-20 18:01:08 +0800

    U

---
+++ lua/plugins/indent.lua
@@ -0,0 +1,22 @@
+local highlight = {
+  "CursorColumn",
+  "Whitespace",
+}
+
+return {
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    event = "LazyFile",
+    opts = {
+      indent = { highlight = highlight, char = "" },
+      whitespace = {
+        highlight = highlight,
+        remove_blankline_trail = false,
+      },
+      scope = {
+        enabled = false,
+      },
+    },
+  },
+}
