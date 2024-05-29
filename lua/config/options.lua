670238f5e67f9ae12f6087728fbae75d21ec1bb6
wp.l <wenpeng.lei@ncsi.com.cn>
2024-05-29 09:22:29 +0800

    chore: Merge

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,10 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-local alpha = function()
-  return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
-end
-
 local function is_unix()
   return package.config:sub(1, 1) == "/"
 end
@@ -25,6 +21,14 @@ if vim.g.neovide then
   vim.g.neovide_cursor_vfx_mode = "pixiedust"
   vim.o.guifont = "Mononoki Nerd Font:h16" -- text below applies for VimScript
 end
+
+-- if vim.g.neovide then
+--     vim.g.neovide_window_blurred = true
+--     vim.g.neovide_show_border = true
+--     vim.g.neovide_transparency = 0.1
+--     vim.g.transparency = 0.6
+--     vim.o.guifont = "Mononoki Nerd Font:h16" -- text below applies for VimScript
+-- end
 
 local opt = vim.opt
 
