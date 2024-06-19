5c82a5258a49b350ebbecbf081dfe61abc8b79c8
wp.l <wenpeng.lei@ncsi.com.cn>
2024-06-19 13:37:54 +0800

    chore: update conform setup

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,3 +1,26 @@
 return {
   "stevearc/conform.nvim",
+  opts = function(_, opts)
+    opts.log_level = vim.log.levels.DEBUG
+
+    opts.formatters_by_ft = opts.formatters_by_ft or {}
+
+    local add_formatter_by_ft = function(ft, formatter)
+      opts.formatter_by_ft[ft] = formatter
+    end
+
+    local supported = {
+      lua = { "stylua" },
+      typescript = {},
+      javascript = {},
+      python = { "isort", "black" },
+      ["*"] = { "trim_whitespace" },
+    }
+
+    for ft, formatter in ipairs(supported) do
+      add_formatter_by_ft(ft, formatter)
+    end
+
+    opts.formatters = opts.formatters or {}
+  end,
 }
