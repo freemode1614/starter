8caf78dcf306bee7f1aa6707edc26cc2db94bb38
freemode <freemode1614@outlook.com>
2024-06-19 20:55:33 +0800

    a

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,26 +1,19 @@
 return {
   "stevearc/conform.nvim",
+  optional = true,
   opts = function(_, opts)
-    opts.log_level = vim.log.levels.DEBUG
+    local local_formatters_by_ft = {
+      typescript = { { "prettierd", "prettier" } },
+      lua = { "stylua" },
+      fish = { "fish_indent" },
+      sh = { "shfmt" },
+      python = { "isort", "black" }
+    }
 
     opts.formatters_by_ft = opts.formatters_by_ft or {}
 
-    local add_formatter_by_ft = function(ft, formatter)
-      opts.formatter_by_ft[ft] = formatter
+    for ft, formatter in ipairs(local_formatters_by_ft) do
+      opts.formatters_by_ft[ft] = formatter
     end
-
-    local supported = {
-      lua = { "stylua" },
-      typescript = {},
-      javascript = {},
-      python = { "isort", "black" },
-      ["*"] = { "trim_whitespace" },
-    }
-
-    for ft, formatter in ipairs(supported) do
-      add_formatter_by_ft(ft, formatter)
-    end
-
-    opts.formatters = opts.formatters or {}
-  end,
+  end
 }
